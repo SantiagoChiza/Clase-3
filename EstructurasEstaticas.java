648fe07e0042f5d23180ad5c99842af0f25119a7
303 - Programacion <303@laboratorios.usbbog.edu.co>
2025-08-13 17:00:53 -0500

    Main: Clase-3

---
+++ EstructurasEstaticas.java
@@ -0,0 +1,8 @@
+public class EstructurasEstaticas {
+    public static void main(String[]arg){
+        int a = 10;
+        int b = 11;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
