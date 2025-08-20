fdf790c9246cfc39efd84aca965143e354c043a9
Santiago Chiquiza <eschiquizac@academia.usbbog.edu.co>
2025-08-20 15:28:36 -0500

    Estructuras Estaticas.py

--- Estructuras Estaticas.py
+++ Estructuras Estaticas.py
@@ -1,1 +1,28 @@
 a = 10
+b = 11
+
+## c, d = 10, 11
+
+suma = a + b
+resta = a - b
+multiplicacion = a * b
+division = a / b
+
+print("La suma es: ", suma)
+print("La resta es: ", resta)
+print("La multiplicación es: ", multiplicacion)
+print("La división es: ", division)
+
+## Otras estructuras de datos
+palabra = "Hola"
+for i in palabra:
+    print (i) ## Las String ocupan menos espacio de memoria, mientras que los char ocupan más.
+
+c = 'H'
+d = 'O'
+e = 'L'
+f = 'A'
+print(c + d + e + f) ## Sumar letras sí es posible en Python. Esto es conocido como concatenar.
+
+x = True
+y = False
